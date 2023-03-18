214804e6f3f49427b341478ac4dad18e181657a2
FrancisRSweb <68391029+FrancisRSweb@users.noreply.github.com>
2023-03-18 09:45:12 +0100

    Create primer.py
    Modif script name

---
+++ primer.py
@@ -0,0 +1,19 @@
+# Primer archivo de python weekend
+
+#lista = ["Roca", "Ruiz", 3, ["Juan", "Gala"]]
+#
+#print(lista[2])
+#print(lista[0])
+#print(lista[1])
+#print(lista[3][1])
+#
+
+# TUPLAS 
+
+tupla = ("Fran", "Gala", "Dobby")
+
+print(tupla)
+print(len(tupla))
+print(tupla[1])
+print(tupla[0])
+print(tupla[2])
